9e67b0cd243c24bda9e557394ac2828585222ff0
Maduwantha <Maduwantha.ka@gmail.com>
2025-07-02 16:16:02 +0530

    Implement the DataBase connection
    I implement the Database connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,18 @@
-console.log("Hello, World bye bye!");
+// Password: sample
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//MiddleWare
+app.use("/",(req,res,next)=>{
+    res.send("Middleware is working");
+})
+
+mongoose.connect("mongodb+srv://Maduwantha:sample@quickcart.0gi7oq0.mongodb.net/")
+.then(()=>console.log("Connected to MongoDB"))
+.then(()=>{
+    app.listen(5000);
+})
+.catch((err)=>console.log((err)));
